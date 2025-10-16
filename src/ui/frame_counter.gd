f9f5f79708e7c8ccb8a21fb0a6741d0284113821
Forest <wilsonforest99@gmail.com>
2025-10-16 17:08:44 -0400

    a

--- src/ui/frame_counter.gd
+++ src/ui/frame_counter.gd
@@ -2,7 +2,7 @@ extends MarginContainer
 
 @export var textLabel: Label
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if not visible:
 		return
 	textLabel.text = str(Engine.get_frames_per_second()) + " FPS"
